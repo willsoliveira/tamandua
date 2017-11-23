e1de6d9a692f53e83caa4a262ea4b3f8884d3609
Will <w.s.oliveira@outlook.com>
2017-11-23 09:40:05 -0300

    mudança da navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,94 +18,42 @@
 <body>
     <div id="app">
     <nav class="navbar navbar-toggleable-md static-top bg-primary navbar-inverse">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="buttton" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item active"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                             <li class="nav-item active"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a href="#" class="nav-link dropdown-toggle"  id="navbarDropdownMenuLink">
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+            <div class="collapse navbar-collapse navbar-inverse" id="navbarText">
+                @guest
+                    <ul class="navbar-nav mr-auto">
+                        
+                    </ul>
+                    <ul class="navbar-nav " >
+                        <li class="nav-item active"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                        <li class="nav-item active"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                    </ul>
+                @else
+                    <span class="nav-item dropdown ">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                             {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                                         Logout
-                                    </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                        <a class="dropdown-item" href="#">outro</a>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-
-                </div>
-
-            </div>
-            
-            
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </span>
+                    
+                 @endguest
         </div>
     </nav>
         @yield('content')
     </div>
-        {{--  <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>  --}}
-
-    
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
